b9d3d92ec8ed358484342c02c99b6831ae2b69d6
Summer <omychic@126.com>
2017-03-10 19:11:02 +0800

    hello!!!!

---
+++ Net News/Net News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Net News
+//
+//  Created by Summer on 17/3/10.
+//  Copyright © 2017年 omychic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
